1de1a3cf80cb01983765da2d75497f5316d27ee9
TakWolf <takwolf@foxmail.com>
2023-02-17 17:36:29 +0800

    字形资源映射

--- build.py
+++ build.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     design_service.verify_glyph_files()
+    alphabet, glyph_file_paths = design_service.collect_glyph_files()
 
 
 if __name__ == '__main__':
