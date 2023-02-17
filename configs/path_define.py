7ae47fb1bb65429afcc10f06168efc53e26b01ca
TakWolf <takwolf@foxmail.com>
2023-02-17 17:40:21 +0800

    补充路径定义配置

--- configs/path_define.py
+++ configs/path_define.py
@@ -4,3 +4,9 @@ project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 assets_dir = os.path.join(project_root_dir, 'assets')
 glyphs_dir = os.path.join(assets_dir, 'glyphs')
+
+build_dir = os.path.join(project_root_dir, 'build')
+outputs_dir = os.path.join(build_dir, 'outputs')
+releases_dir = os.path.join(build_dir, 'releases')
+
+docs_dir = os.path.join(project_root_dir, 'docs')
