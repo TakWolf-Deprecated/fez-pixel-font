308f0af4d084389eab098483566d4c6214f9e658
TakWolf <takwolf@foxmail.com>
2023-02-17 17:41:18 +0800

    添加文件操作工具类

--- build.py
+++ build.py
@@ -1,11 +1,15 @@
 import logging
 
+from configs import path_define
 from services import design_service
+from utils import fs_util
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
+    fs_util.delete_dir(path_define.build_dir)
+
     design_service.verify_glyph_files()
     alphabet, glyph_file_paths = design_service.collect_glyph_files()
 
