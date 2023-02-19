13979ae47a2bdfe8a43fc44f5016e9834bd393fc
TakWolf <takwolf@foxmail.com>
2023-02-20 00:50:49 +0800

    完成演示页面

---
+++ docs/js/index.js
@@ -0,0 +1,33 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: '' +
+                'FROM OUT OF NOWHERE\n' +
+                'IMPOSSIBLE VISITORS\n' +
+                'OUR BENEFACTORS\n' +
+                'WATCHING OVER US\n' +
+                'IN THE HIDDEN FOLDS OF SPACE\n' +
+                'IN FRONT OF BEHIND\n' +
+                'GIVE THE GOLDEN GIFT\n' +
+                'A DEEP REVELATION\n' +
+                'OUR EYES WIDE OPEN\n' +
+                'SHAPES TO TESSELLATE\n' +
+                'WITH SACRED GEOMETRY\n' +
+                'AN EMPIRE TO BUILD\n' +
+                'A NEW PERSPECTIVE\n' +
+                'THINGS UNSEENS BUT ALWAYS THERE\n' +
+                'A NEW DIRECTION\n' +
+                'THE HEXAHEDRON\n' +
+                'THE SIXTY FOUR BIT NAME OF GOD\n' +
+                'THE POINT OF ORIGIN\n' +
+                'A PATTERN A CODE\n' +
+                'A DEEP UNDERSTANDING\n' +
+                'A GATE TO THE STARS\n' +
+                'ALL OF TIME AND SPACE\n' +
+                'AND THE SPACE OUTSIDE OF SPACE\n' +
+                'WHERE DOES IT END'
+        }
+    }
+}).mount('#app')
